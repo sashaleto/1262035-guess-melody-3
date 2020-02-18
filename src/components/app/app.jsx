5c1b98d2f2e370b0811454dd310bc1b7283088fa
Marochkina <marochkina@gfcc.su>
2020-02-18 19:25:36 +0300

    Рендер приветственной страницы

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
+const App = () => {
+  return (
+    <WelcomeScreen/>
+  );
+};
+
+export default App;
