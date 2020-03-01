ad2cd76c8b5ee019ba69f0ad7099e315889b5255
sashaleto <aleks.nesc@gmail.com>
2020-03-01 14:14:51 +0300

    Добавлено переключение по страницам приложения для разработки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
