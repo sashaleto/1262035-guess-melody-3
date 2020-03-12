1972c3d4387cb433d950a1dad6c957df6fedbd42
Marochkina <marochkina@gfcc.su>
2020-03-12 15:04:46 +0300

    Добавление обработчика на выбор варианта ответа в вопросе об артисте

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const ArtistQuestionScreen = ({question}) => {
+const ArtistQuestionScreen = ({question, onAnswer}) => {
   const {answers, song} = question;
   return (
     <section className="game game--artist">
@@ -37,7 +37,12 @@ const ArtistQuestionScreen = ({question}) => {
         <form className="game__artist">
           {answers.map((answer, i) => (
             <div key={answer.artist} className="artist">
-              <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`} id={`answer-${i}`}/>
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`} id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
               <label className="artist__name" htmlFor={`answer-${i}`}>
                 <img className="artist__picture" src={answer.picture} alt={answer.artist}/>
                 {answer.artist}
@@ -61,5 +66,6 @@ ArtistQuestionScreen.propTypes = {
       picture: PropTypes.string.isRequired,
     })).isRequired,
   }).isRequired,
+  onAnswer: PropTypes.func.isRequired,
 };
 export default ArtistQuestionScreen;
