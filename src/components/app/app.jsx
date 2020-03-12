98bd67e54c908d6183de9b0ab51f1184d1cf2086
Marochkina <marochkina@gfcc.su>
2020-03-12 15:02:53 +0300

    Описание состояния приложения в зависимости от типа игры

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,30 +4,84 @@ import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-const welcomeButtonHandler = () => {};
+import {GameType} from '../../constants';
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      step: -1,
+    };
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0,
+            });
+          }}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
+
+    return null;
   }
 
   render() {
-    const {errorsCount, questions} = this.props;
+    const {questions} = this.props;
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <WelcomeScreen
-              errorsCount={errorsCount}
-              onWelcomeButtonClick={welcomeButtonHandler}
-            />
+            {this._renderGameScreen()}
           </Route>
           <Route exact path="/dev-artist">
-            <ArtistQuestionScreen question={questions[1]}/>
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
           </Route>
           <Route exact path="/dev-genre">
-            <GenreQuestionScreen question={questions[0]}/>
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
           </Route>
         </Switch>
       </BrowserRouter>
