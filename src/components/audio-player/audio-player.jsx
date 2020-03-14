ccc36385c1d6af11426095c9d60044eef0331944
sashaleto <aleks.nesc@gmail.com>
2020-03-14 13:05:03 +0300

    Добавление ref для прямого обращения к DOM-элементу audio

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,9 +1,11 @@
-import React, {Fragment, PureComponent} from 'react';
+import React, {Fragment, PureComponent, createRef} from 'react';
 import PropTypes from "prop-types";
 
 class AudioPlayer extends PureComponent {
   constructor(props) {
     super(props);
+
+    this._audioRef = createRef();
 
     this.state = {
       progress: 0,
@@ -14,33 +16,35 @@ class AudioPlayer extends PureComponent {
 
   componentDidMount() {
     const {src} = this.props;
+    const audio = this._audioRef.current;
 
-    this._audio = new Audio(src);
+    audio.src = src;
 
-    this._audio.oncanplaythrough = () => this.setState({
+    audio.oncanplaythrough = () => this.setState({
       isLoading: false,
     });
 
-    this._audio.onplay = () => this.setState({
+    audio.onplay = () => this.setState({
       isPlaying: true,
     });
 
-    this._audio.onpause = () => this.setState({
+    audio.onpause = () => this.setState({
       isPlaying: false,
     });
 
-    this._audio.ontimeupdate = () => this.setState({
-      progress: this._audio.currentTime
+    audio.ontimeupdate = () => this.setState({
+      progress: audio.currentTime
     });
   }
 
   componentWillUnmount() {
-    this._audio.oncanplaythrough = null;
-    this._audio.onplay = null;
-    this._audio.onpause = null;
-    this._audio.ontimeupdate = null;
-    this._audio.src = ``;
-    this._audio = null;
+    const audio = this._audioRef.current;
+
+    audio.oncanplaythrough = null;
+    audio.onplay = null;
+    audio.onpause = null;
+    audio.ontimeupdate = null;
+    audio.src = ``;
   }
 
   render() {
@@ -54,17 +58,18 @@ class AudioPlayer extends PureComponent {
           onClick={() => this.setState({isPlaying: !this.state.isPlaying})}
         />
         <div className="track__status">
-          <audio />
+          <audio ref={this._audioRef} />
         </div>
       </Fragment>
     );
   }
 
   componentDidUpdate() {
+    const audio = this._audioRef.current;
     if (this.state.isPlaying) {
-      this._audio.play();
+      audio.play();
     } else {
-      this._audio.pause();
+      audio.pause();
     }
   }
 }
